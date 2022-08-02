2ac56b1cb4426549daac1f0ac290e5bba2ab1ca0
Duong (Zach) Nguyen <novak@novakcgx.me>
2022-08-03 01:42:19 +0700

    ArrayQueue: dequeue is now O(1)

--- src/main/lists/ArrayQueue.java
+++ src/main/lists/ArrayQueue.java
@@ -2,27 +2,46 @@
  * An implementation of queue using a static array.
  *
  * <p>
+ * This implementation uses the cyclic buffer by storing the indices of the rear
+ * and the front of the queue to achieve O(1) complexity on all operations.
+ *
+ * <p>
  * Operations and their time complexities are:
  *   <ul>
  *     <li><code>enqueue</code>: O(1)</li>
- *     <li><code>dequeue</code>: O(n)</li>
+ *     <li><code>dequeue</code>: O(1)</li>
  *     <li><code>peek</code>: O(1)</li>
  *   </ul>
  */
 public class ArrayQueue<T> {
   private Object[] array;
   private int length;
+  private int frontIndex;
+  private int rearIndex;
 
-  public ArrayQueue(int maxLength) {
-    this.array = new Object[maxLength];
+  public ArrayQueue(int capacity) {
+    this.array = new Object[capacity];
     this.length = 0;
+    this.frontIndex = 0;
+    this.rearIndex = 0;
+  }
+
+  public int getLength() {
+    return this.length;
+  }
+
+  public int getCapacity() {
+    return this.array.length;
   }
 
   public ArrayQueue<T> enqueue(T data) {
     if (this.length == this.array.length)
       return this;
 
-    this.array[this.length++] = data;
+    this.rearIndex = (this.rearIndex + 1) % this.array.length;
+    this.array[this.rearIndex] = data;
+
+    this.length++;
     return this;
   }
 
@@ -30,29 +49,15 @@ public class ArrayQueue<T> {
     if (this.length == 0)
       return this;
 
-    int i;
-    for (i = 1; i < this.length; i++)
-      this.array[i - 1] = this.array[i];
+    this.array[this.frontIndex] = null;
+    this.frontIndex = (this.frontIndex + 1) % this.array.length;
 
-    this.array[i - 1] = null;
     this.length--;
     return this;
   }
 
   @SuppressWarnings("unchecked")
   public T peek() {
-    return (T) this.array[0];
-  }
-
-  public static void main(String[] args) {
-    ArrayQueue<Integer> queue = new ArrayQueue<Integer>(10);
-
-    for (int i = 0; i < 15; i++)
-      queue.enqueue(i);
-
-    for (int i = 0; i < 20; i++) {
-      System.out.println(queue.peek());
-      queue.dequeue();
-    }
+    return (T) this.array[this.frontIndex];
   }
 }
